f7b7e87123065df899a26247f1c2d424310cd973
Josh Kuss <joshua.d.kuss@gmail.com>
2020-05-27 10:48:31 -0500

    little DB changes

--- database.sql
+++ database.sql
@@ -9,19 +9,26 @@ CREATE TABLE "user" (
     "password" VARCHAR (80) NOT NULL,
     "zip_code" INT,
     "child_age" INT,
-    "faves" INT
+    "faves" VARCHAR (250)
 );
 
 CREATE TABLE "playground" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (80) UNIQUE NOT NULL,
     "address" VARCHAR (80) NOT NULL,
-    "img_url" VARCHAR (250),
-    "description" VARCHAR (250)
+    "img_url" VARCHAR (350),
+    "description" VARCHAR (250),
+    "kiddie_friendly" BOOLEAN default FALSE
 );
+
 
 CREATE TABLE "favorites" (
     "id" SERIAL PRIMARY KEY,
     "user_faves" INT,
     "pg_id" INT
 );
+
+INSERT INTO "playground" ("name", "address", "img_url", "description", "kiddie_friendly")
+VALUES ('Casey Lake Park', '2101 17th Ave E, North Saint Paul 55109', 
+'https://www.google.com/maps/uv?hl=en&pb=!1s0x52b2d3eec326ab19%3A0xb5102d5898e4e913!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNafh6sog1g3UsoNe9pA17lo5inUmMA9YQC3AzX%3Dw260-h175-n-k-no!5splayground%2055109%20-%20Google%20Search!15sCgIgAQ&imagekey=!1e10!2sAF1QipNafh6sog1g3UsoNe9pA17lo5inUmMA9YQC3AzX#',
+'Sprawling, modern, well-maintained park in North Saint Paul. Public restrooms, walking path, fishing pond, custom swings, nearby baseball fields. Frequent ice cream truck visits', TRUE);
